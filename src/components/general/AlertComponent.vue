3f0a962bca0e680d0b5703c70e4706183f253332
Tobias Forneberg <tobias.forneberg@student.uni-siegen.de>
2019-10-30 00:46:39 +0100

    added AlertComponent

---
+++ src/components/general/AlertComponent.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { Component, Prop, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class AlertComponent extends Vue {
+        @Prop({default: false}) public show : boolean;
+        @Prop({default: false}) public error : boolean;
+        @Prop({default: true}) public success : boolean;
+
+        mounted() {
+         }
+    }
+</script>
+
+<template>
+    <transition name="fade">
+        <div v-if="show" class="text-center alert" :class="error ? 'alert-danger' : success ? 'alert-success' : ''" role="alert">
+            <slot></slot>
+        </div>
+    </transition>
+</template>
+
+<style scoped lang="scss">
+    .fade-enter-active, .fade-leave-active {
+        transition: all 0.5s;
+    }
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
+    }
+</style>
